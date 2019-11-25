0c4b8aa4f0dc7e156c50f338dfaba5e0a4ee378e
Rafał Szulejko <rafal.szulejko@gmail.com>
2019-11-25 23:18:10 +0100

    Replaced 'read_case.m' with more universal function with variable input and output.

---
+++ read_tab.m
@@ -0,0 +1,89 @@
+function varargout = read_tab(filename, varargin)
+    %% safety checks and setup
+    narginchk(2, inf);
+    
+    if nargin - 1 ~= nargout
+        error("Number of required variables and output length don't match.")
+    end
+
+    file = fopen(filename);
+    
+    line = fgetl(file);
+    
+    vars_read = 0;
+    %% skip all comments
+    while line(1) == '#'
+        line = fgetl(file);
+    end
+    %% read matrices
+    while ~feof(file)
+        tabinfo = split(line, ' ');
+        
+        tabname = cell2mat(tabinfo(1));
+        
+        if ~ismember(tabname, varargin)
+            line = fgetl(file);
+            while ~isempty(line) && ~feof(file) && ~isletter(line(1))
+                line = fgetl(file);
+            end
+            
+            continue
+        end
+        
+        tab_pos = find(cellfun(@(x) isequal(x, tabname), varargin));
+        
+        tab_h = str2num(cell2mat(tabinfo(2)));
+        tab_w = str2num(cell2mat(tabinfo(3)));
+        
+        tempfilename = strcat(tabname, '.dat');
+        tempfile = fopen(tempfilename, 'w');
+        
+        line = fgetl(file);
+        
+        while line(1) == '#'
+            line = fgetl(file);
+        end
+
+        while ~isempty(line) && ~feof(file) && line(1) == ' '
+            fprintf(tempfile, line);
+            fprintf(tempfile, '\n');
+            line = fgetl(file);
+        end
+        
+        fprintf(tempfile, line);
+        
+        if ~feof(file)
+            line = fgetl(file);
+        end
+        
+        fclose(tempfile);
+        
+        reshapedtab = readmatrix(tempfilename);
+
+        varargout{tab_pos} = reshape(...
+            trim_nans(...
+            reshape (reshapedtab', [numel(reshapedtab) 1])'), [tab_w, tab_h]).';
+
+        delete(tempfilename);
+        
+        vars_read = vars_read + 1;
+        
+        if vars_read == nargin - 1
+            break
+        end
+    end
+    
+    if vars_read < nargin - 1
+        error("Not all requested variables were found in the file.");
+    end
+    
+    fclose(file);
+end
+
+function res = trim_nans(vec)
+    while isnan(vec(end))
+        vec = vec(1:(length(vec)-1));
+    end
+
+    res = vec;
+end
